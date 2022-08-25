1feaa0565acc985997d1394982d47caef91526a3
JakeSchlei <jakeschlei@gmail.com>
2022-08-24 21:51:54 -0700

    added functions to obtain city, weather and forecast data

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,34 @@ function findCity() {
     fetch(cityUrl)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
+        console.log(`findCity data: ${JSON.stringify(data)}`);
+        return getCurrentWeather({lat: data[0].lat, lon: data[0].lon, city: data[0].name});
+    
     });
 
 }
+
+function getCurrentWeather({lat, lon, city}) {
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(currentWeatherUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(`getCurrentWeather data: ${JSON.stringify(data)}`);
+        // to do: update DOM
+        return getForecast({lat, lon, city}) ;
+    });
+
+}
+
+function getForecast({lat, lon, city}) {
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(forecastUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(`getForecast data: ${JSON.stringify(data)}`);
+    });
+
+
+}
+
 
