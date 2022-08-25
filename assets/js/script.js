3e627c41935ce737bfe173e7a89903bac4736aa1
JakeSchlei <jakeschlei@gmail.com>
2022-08-24 17:02:47 -0700

    added more css styling and a function to find the city when searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 const apiKey = `c2f95353b2d0391fb3a27563cb3c086f`;
+const searchBtn = document.querySelector('#search-btn');
+const citySearch = document.querySelector('#city');
+
+
+searchBtn.addEventListener('click', findCity)
+function findCity() {
+    const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearch.value}&limit=5&appid=${apiKey}`;
+    fetch(cityUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    });
+
+}
+
