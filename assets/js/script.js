2c489c14d737886ec7f637bc49d0ca22a257b586
JakeSchlei <jakeschlei@gmail.com>
2022-08-24 22:46:26 -0700

    added temp wind and humidity to main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const apiKey = `c2f95353b2d0391fb3a27563cb3c086f`;
 const searchBtn = document.querySelector('#search-btn');
 const citySearch = document.querySelector('#city');
+const currentWeather = document.querySelector('#current-weather');
 
 
-searchBtn.addEventListener('click', findCity)
-function findCity() {
-    const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearch.value}&limit=5&appid=${apiKey}`;
+searchBtn.addEventListener('click', () => findCity(citySearch.value))
+function findCity(city) {
+    const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
     fetch(cityUrl)
     .then(res => res.json())
     .then(data => {
@@ -17,19 +18,32 @@ function findCity() {
 }
 
 function getCurrentWeather({lat, lon, city}) {
-    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     fetch(currentWeatherUrl)
     .then(res => res.json())
     .then(data => {
         console.log(`getCurrentWeather data: ${JSON.stringify(data)}`);
+        const createDate = new Date(data.dt * 1000).toString().split(' ').slice(0, 4).join(' ');
+
+        currentWeather.innerHTML =
+        `<h3>${city} (${createDate})</h3>
+        <div class='weather-details'>
+            <p>Temperature: ${parseInt(data.main.temp)}°</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Wind: ${parseInt(data.wind.speed)} MPH</p>
+
+        `
+        
         // to do: update DOM
         return getForecast({lat, lon, city}) ;
+
+        
     });
 
 }
 
 function getForecast({lat, lon, city}) {
-    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     fetch(forecastUrl)
     .then(res => res.json())
     .then(data => {
@@ -38,5 +52,7 @@ function getForecast({lat, lon, city}) {
 
 
 }
+
+findCity("Portland");
 
 
